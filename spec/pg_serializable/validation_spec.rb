2d28f8aec2386ca5f03d1303e322519cf6af1904
matthewjf <matthewjf@gmail.com>
2019-05-22 18:02:01 -0700

    added some validation specs

--- spec/pg_serializable/validation_spec.rb
+++
@@ -1,7 +0,0 @@
-require 'spec_helper'
-
-RSpec.describe "validations" do
-  it "works" do
-    expect(true).to eq(false)
-  end
-end
---
+++ spec/pg_serializable/validations_spec.rb
@@ -0,0 +1,12 @@
+require 'spec_helper'
+
+RSpec.describe "validations" do
+  context "attributes" do
+    it "must have column"
+  end
+
+  context "associations" do
+    it "must have the association"
+    it "must not have cycles"
+  end
+end

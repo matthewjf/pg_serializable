f6b295f563a4a117e213b2d6f2e65f4a00db770d
matthewjf <matthewjf@gmail.com>
2019-07-07 10:50:36 -0700

    fix tests

--- spec/pg_serializable/validation_spec.rb
+++ spec/pg_serializable/validation_spec.rb
@@ -3,30 +3,26 @@ require 'spec_helper'
 RSpec.describe "validation" do
   context "attributes" do
     it "raises error when column is missing" do
-      # expect do
-        class Product < ApplicationRecord
-          serializable do
-            default do
-              attribute :test
-            end
+      class Product < ApplicationRecord
+        serializable do
+          default do
+            attribute :test
           end
         end
-      # end.to raise_error(PgSerializable::AttributeError)
+      end
 
       expect { PgSerializable.validate_traits! }.to raise_error(PgSerializable::AttributeError)
     end
 
     it "doesn't raise an error when column exists" do
-      # expect do
-        class Product < ApplicationRecord
-          serializable do
-            default { attribute :name }
-            trait :other do
-              attributes :name, :product_type, :label_id
-            end
+      class Product < ApplicationRecord
+        serializable do
+          default { attribute :name }
+          trait :other do
+            attributes :name, :product_type, :label_id
           end
         end
-      # end.to_not raise_error
+      end
 
       expect { PgSerializable.validate_traits! }.to_not raise_error
     end
@@ -34,37 +30,35 @@ RSpec.describe "validation" do
 
   context "associations" do
     it "raises error when association doesn't exist" do
-      # expect do
-        class Product < ApplicationRecord
-          serializable do
-            default do
-              belongs_to :nonexistant
-            end
+      class Product < ApplicationRecord
+        serializable do
+          default do
+            belongs_to :nonexistant
           end
         end
-      # end.to raise_error(PgSerializable::AssociationError)
+      end
+
       expect { PgSerializable.validate_traits! }.to raise_error(PgSerializable::AssociationError)
     end
 
     it "raises error when cycles exist" do
-      #expect do
-        class Label < ApplicationRecord
-          has_many :products
-          serializable do
-            default do
-              has_many :products
-            end
+      class Label < ApplicationRecord
+        has_many :products
+        serializable do
+          default do
+            has_many :products
           end
         end
-        class Product < ApplicationRecord
-          belongs_to :label
-          serializable do
-            default do
-              belongs_to :label
-            end
+      end
+      class Product < ApplicationRecord
+        belongs_to :label
+        serializable do
+          default do
+            belongs_to :label
           end
         end
-      # end.to raise_error(PgSerializable::AssociationError)
+      end
+
       expect { PgSerializable.validate_traits! }.to raise_error(PgSerializable::AssociationError)
     end
   end
